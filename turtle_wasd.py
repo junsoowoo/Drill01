ab4c180fcf206ba60be8aa8f9116490142c1a477
Junsoo Han <hanjs991124@gmail.com>
2023-09-13 22:01:39 +0900

    라이브러리 추가1, 변경

--- turtle_wasd.py
+++ turtle_wasd.py
@@ -3,26 +3,34 @@ import turtle
 turtle.shape('turtle')
 speed=50
 x,y=0,0
+
 def restart():
     turtle.reset()
-
+    
 def move_up():
     global y
     y += speed
+    turtle.sety(y)
+    
 def move_down():
     global y
     y -= speed
+    turtle.sety(y)
+    
 def move_left():
     global x
     x -= speed
+    turtle.setx(x)
+    
 def move_right():
     global x
     x += speed
+    turtle.setx(x)
 
 turtle.onkey(move_up,'w')
 turtle.onkey(move_down,'s')
 turtle.onkey(move_left,'a')
 turtle.onkey(move_right,'d')
-turtle.onkey(restart,'Esc')
+turtle.onkey(restart,'Escape')
 turtle.listen()
 turtle.mainloop
