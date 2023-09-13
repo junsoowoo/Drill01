db74c21ba4b830d311cb0be4db054d0a0f847f86
Junsoo Han <hanjs991124@gmail.com>
2023-09-13 21:38:06 +0900

    speed정정, x,y좌표 초기화

--- turtle_wasd.py
+++ turtle_wasd.py
@@ -1,22 +1,23 @@
 import turtle
 
 turtle.shape('turtle')
-
+speed=50
+x,y=0,0
 def restart():
     turtle.reset()
 
 def move_up():
     global y
-    
+    y += speed
 def move_down():
     global y
-     
+    y -= speed
 def move_left():
     global x
-     
+    x -= speed
 def move_right():
     global x
-
+    x += speed
 
 turtle.onkey(move_up,'w')
 turtle.onkey(move_down,'s')
